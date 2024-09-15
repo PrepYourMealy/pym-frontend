9dc062a07f97c96e7afa565f0fde09c90e2a5495
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-15 23:05:55 +0200

    new menu ui

--- src/app/dashboard/menu/page.tsx
+++ src/app/dashboard/menu/page.tsx
@@ -52,7 +52,7 @@ export default function ShoppingListPage() {
         };
 
         return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
                 {dayKeys.map((key) => (
                     <div key={key}>
                        <MenuDayCard day={menu[key]!} dayName={dayMap[key]!} selectedRecipe={selectedRecipe} setSelectedRecipe={setSelectedRecipe} />
