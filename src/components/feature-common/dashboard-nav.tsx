483fcf8dab1fad094ad84436a79577a5d95fce4c
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-16 12:53:46 +0200

    change layouting

--- src/components/feature-common/dashboard-nav.tsx
+++ src/components/feature-common/dashboard-nav.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export function DashboardNav({title}: Props) {
     return <div className="flex w-full flex-row justify-between items-center">
-        <div className="flex flex-row gap-4 items-center">
+        <div className="flex flex-row gap-2 items-center">
             <SidebarTrigger/>
-            <h1 className="text-lg font-body">{title}</h1>
+            <h1 className="text-md font-body">{title}</h1>
         </div>
 
         <div className="flex flex-row gap-4 items-center">
