3962c99933309fd5788e36f6401bbf1cd747abfa
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-16 10:01:12 +0200

    add regenerate button to menu page

--- src/app/dashboard/menu/page.tsx
+++ src/app/dashboard/menu/page.tsx
@@ -52,12 +52,20 @@ export default function ShoppingListPage() {
         };
 
         return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-                {dayKeys.map((key) => (
-                    <div key={key}>
-                       <MenuDayCard day={menu[key]!} dayName={dayMap[key]!} selectedRecipe={selectedRecipe} setSelectedRecipe={setSelectedRecipe} />
-                    </div>
-                ))}
+            <div>
+                <div className="flex flex-row-reverse items-center py-2">
+                    <Button onClick={regenerateMenu}>
+                        Neu generieren
+                    </Button>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+                    {dayKeys.map((key) => (
+                        <div key={key}>
+                            <MenuDayCard day={menu[key]!} dayName={dayMap[key]!} selectedRecipe={selectedRecipe}
+                                         setSelectedRecipe={setSelectedRecipe}/>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     } else {
