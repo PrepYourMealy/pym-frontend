9dc17286d44a949ae55c5b91a68ad3ab6e0d4fbd
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-26 19:42:39 +0200

    add new page layout

--- src/components/feature-discounts/discount-list-item.tsx
+++ src/components/feature-discounts/discount-list-item.tsx
@@ -10,7 +10,7 @@ interface Props {
 export function DiscountListItem({item, handleAddToList}: Props) {
     return (<div className="flex items-center py-4 max-w-full">
         <img src={item.img!} alt={item.name!} className="w-20 h-20 object-cover rounded-md mr-4"/>
-        <div className="flex-grow">
+        <div>
             <h3 className="font-bold text-lg text-wrap">{item.name}</h3>
             <p className="text-sm text-gray-600 line-clamp-2">{item.description}</p>
             <div className="flex items-center mt-2">
